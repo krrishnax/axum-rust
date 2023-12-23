6000217c8ac61ba8acc3e434c96eff36e9b15248
isamyakt <isamyakt17@gmail.com>
2023-12-23 15:05:49 +0530

    implemented static route which displays content-type: text/x-rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![allow(unused)] // For beginning only.
 
-use axum::{Router, routing::get, response::{Html, IntoResponse}, extract::{Query, Path}};
+use axum::{Router, routing::{get, get_service}, response::{Html, IntoResponse}, extract::{Query, Path}};
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/:name", get(handler_hello2));
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     // region:   --- Start Server
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
@@ -16,9 +17,21 @@ async fn main() {
 
     println!("->> LISTENING on {:?}\n", listener);
 
-    axum::serve(listener, routes_hello)
+    axum::serve(listener, routes_all)
         .await
         .unwrap()
+}
+
+// e.g: `http://localhost:8080/src/main.rs`
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
+// region:   --- Router Hello
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
 }
 
 // region:   --- Handler Hello
@@ -27,7 +40,7 @@ struct HelloParams {
     name: Option<String>
 }
 
-// e.g: `/hello?name=Krrishna`
+// e.g: `http://localhost:8080/hello?name=Krrishna`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
 
@@ -37,11 +50,9 @@ async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     Html(format!("Hello <strong>{name}</strong>"))
 }
 
+// e.g: `http://localhost:8080/hello2/Palavi`
 async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
 
     Html(format!("Hello <strong>{name}</strong>"))
 }
-
-// cargo watch -q -c -w src/ -x run (backend)
-// cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"  (client)
